abf9bd21071c8b905b41585e870ec1a07cc80a40
Rimian Perkins <hello@rimian.com.au>
2020-02-26 23:21:44 +1100

    filter tiles out of bounds from moore algorithm

--- src/neighborhood.js
+++ src/neighborhood.js
@@ -1,5 +1,21 @@
 const moore = require('moore')
 
-module.exports = function neighborhood(tiles, x, y) {
-  return moore().map((xy) => tiles[xy[1] + y][xy[0] + x])
+module.exports = function neighborhood(x, y, tiles) {
+  const outOfBounds = (x, y) => {
+    const height = tiles.length
+    return (x < 0 || y < 0 || y + 1 > height) ? true : false
+  }
+
+  const neighbors = moore(1, 2).map((xy) => {
+    const x1 = xy[0] + x
+    const y1 = xy[1] + y
+
+    if(outOfBounds(x1, y1)) {
+      return
+    }
+
+    return tiles[y1][x1]
+  })
+
+  return neighbors.filter(t => t)
 }
--- test/neighborhood.test.js
+++ test/neighborhood.test.js
@@ -1,19 +1,64 @@
 const neighborhood = require('../src/neighborhood')
 
-describe('neighborhood', () => {
-  test('neighbours', () => {
-    const tiles = [
-      [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      [10,11,12,13,14,15,16,17,18,19],
-      [20,21,22,23,24,25,26,27,28,29],
-      [30,31,32,33,34,35,36,37,38,39]
-    ]
+const tiles = [
+  [ 0, 1, 2, 3, 4, 5],
+  [10,11,12,13,14,15],
+  [20,21,22,23,24,25],
+  [30,31,32,33,34,35]
+]
 
-    expect(neighborhood(tiles, 4, 1)).toEqual(
+describe('neighborhood', () => {
+  test('neighbors of a middle tile', () => {
+    expect(neighborhood(4, 1, tiles)).toEqual(
       [
-        3,  4,  5,
+         3,  4,  5,
         13,     15,
         23, 24, 25
+      ]
+    )
+  })
+
+  test('neighbors of the top left', () => {
+    expect(neighborhood(0, 0, tiles)).toEqual(
+      [
+             1,
+        10, 11
+      ]
+    )
+  })
+
+  test('neighbors of the top middle', () => {
+    expect(neighborhood(2, 0, tiles)).toEqual(
+      [
+         1,      3,
+        11, 12, 13
+      ]
+    )
+  })
+
+  test('neighbors of the bottom left', () => {
+    expect(neighborhood(0, 3, tiles)).toEqual(
+      [
+        20, 21,
+            31
+      ]
+    )
+  })
+
+  test('neighbors of the bottom middle', () => {
+    expect(neighborhood(2, 3, tiles)).toEqual(
+      [
+        21, 22, 23,
+        31,     33
+      ]
+    )
+  })
+
+  test('neighbors of the bottom right', () => {
+    expect(neighborhood(5, 3, tiles)).toEqual(
+      [
+        24, 25,
+        34
       ]
     )
   })
